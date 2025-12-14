c73e132385b07c075685c406a5b558bd63d2f85a
Abhay-0103 <ab0321054@gmail.com>
2025-12-14 15:01:45 +0530

    add LoginForm component for user authentication

---
+++ client/components/login-form.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+// Global Import
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+// Local Import
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+import { authClient } from "@/lib/auth-client";
+
+export const LoginForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-6 justify-center items-center">
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <Image src={"/login.png"} alt="Login" height={150} width={150} />
+        <h1 className="text-6xl font-extrabold text-indigo-400">
+          Welcome Back! to AI-Powered CLI
+        </h1>
+        <p className="text-base font-medium text-zinc-400">
+          Login to your account for allowing device flow
+        </p>
+      </div>
+
+      <Card className="border-dashed border-2">
+        <CardContent>
+            
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
